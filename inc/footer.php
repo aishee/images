a3ee386d465568bd8d21e1577d0872c721a5cff9
Aishee Nguyen <loverhakai@gmail.com>
2015-05-13 11:40:48 +0700

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($in_script)) {
+	exit('Not directly');
+}
+?>
+</div>
+<div id="footer">
+	<a href="login.php">Admin login</a>
+
+</div>
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/upload.js"></script>
+</body>
+</html>
