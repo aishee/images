6d0541f7a438b90db70688e2f633281c2fefdac5
Aishee Nguyen <loverhakai@gmail.com>
2015-05-13 11:32:34 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+//Name of site
+$site_name = 'mysite';
+//URL of site
+$site_url  = '#';
+//Path of source
+$script_path = 'path'
+
+//admin-username
+$admin_user = 'admin';
+//Password 
+$admin_password = 'password';
+//contact of  site
+$email_contact = 'email@contact.com';
+//Report email
+$email_report = 'email@report.com';
+//Seo friendly
+$seo_friends = true;
+
+//Database
+$db_server = 'localhost';
+$db_user = 'admin';
+$db_pass = '123456789';
+$db_port = '3306';
+
+//max size
+$file_size = '2048000';
+$file_allow = [	
+'png',
+'zip',
+'jpg',
+'jpeg',
+'bmp'
+];
+?>
