7e200ad96d8955c03f2dfc30343a354a977f26e3
Aishee Nguyen <loverhakai@gmail.com>
2015-05-19 21:41:43 +0700

    Create contact.php

---
+++ inc/contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!isset($in_script))
+{
+	exit('you are not allowed to access this page directly');
+}
+
+?>
+
+<div class="box">
+
+	<p class="title">Contact</p>
+
+	<p>If you would like to contact us, please email <?php echo $contact_email; ?></p>
+
+</div>
